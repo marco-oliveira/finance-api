47aa2265cb3de598bd909766ff35904602927cb7
marco-oliveira <marco-oliveira@live.com>
2019-11-01 00:11:01 -0300

    Add Create new resource in category

--- src/main/java/com/finance/api/resource/CategoryResource.java
+++ src/main/java/com/finance/api/resource/CategoryResource.java
@@ -3,10 +3,12 @@ package com.finance.api.resource;
 import com.finance.api.model.Category;
 import com.finance.api.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,5 +21,17 @@ public class CategoryResource {
     @GetMapping
     public List<Category> find() {
         return categoryRepository.findAll();
+    }
+
+    @PostMapping
+    private ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+        Category categorySaved = categoryRepository.save(category);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(categorySaved.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categorySaved);
     }
 }
