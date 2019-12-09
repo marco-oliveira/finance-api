c456b57d3c88ed5ea10fc73c9ba48180c8daf002
Marco Antônio <marco-oliveira@live.com>
2019-12-08 23:21:58 -0200

    Create model for Person

---
+++ src/main/java/com/finance/api/model/Person.java
@@ -0,0 +1,69 @@
+package com.finance.api.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String name;
+
+    @NotNull
+    private Boolean active;
+
+    @Embedded
+    private PersonAddress personAddress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public PersonAddress getPersonAddress() {
+        return personAddress;
+    }
+
+    public void setPersonAddress(PersonAddress personAddress) {
+        this.personAddress = personAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
